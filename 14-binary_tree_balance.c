ee7a04219aaa70b8b91965235f090b7162f6feca
root <smsmelshamey90@gmail.com>
2024-02-28 09:48:50 -0800

    web infrastructure

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node
+ * Return: integer
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+
+	return (0);
+}
